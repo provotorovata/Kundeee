d821f3c8bb1abf62c75c2b1939f5bcefa275edb6
provotorovata <provotorova.newpenta@gmail.com>
2018-08-29 23:34:04 +0300

    first lesson

---
+++ src/FirstLesson/main.java
@@ -0,0 +1,9 @@
+package FirstLesson;
+
+public class main {
+    public static void main(String[] args) {
+        int sm = 256;
+        int meters= sm/100;
+        System.out.println(meters);
+    }
+}
